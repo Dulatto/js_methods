a21eda66c30f45ec17d834fa7dfc6ad4a0b39c20
Dulatto <dulat.omarov@gmail.com>
2022-01-04 22:29:15 -0500

    array methods

--- methods.js
+++ methods.js
@@ -10,3 +10,15 @@ const array2 = ['d', 'e', 'f'];
 const array3 = array1.concat(array2);
 
 //console.log(array3);
+
+// copyWithin() method
+
+const array7 = ['a', 'b', 'c', 'd', 'e'];
+
+// copy to index 0 the element at index 3
+console.log(array7.copyWithin(0, 3, 4));
+// expected output: Array ["d", "b", "c", "d", "e"]
+
+// copy to index 1 all elements from index 3 to the end
+console.log(array7.copyWithin(1, 3));
+// expected output: Array ["d", "d", "e", "d", "e"]
