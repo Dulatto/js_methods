0aedc2a3d4b1028fdcff6a449105d116479ff556
Dulatto <dulat.omarov@gmail.com>
2022-01-04 22:34:27 -0500

    array methods

--- methods.js
+++ methods.js
@@ -22,3 +22,28 @@ console.log(array7.copyWithin(0, 3, 4));
 // copy to index 1 all elements from index 3 to the end
 console.log(array7.copyWithin(1, 3));
 // expected output: Array ["d", "d", "e", "d", "e"]
+
+
+// entries() method
+// The entries() method returns a new Array Iterator
+// object that contains the key/value pairs for each index in the array.
+
+const array8 = ['a', 'b', 'c'];
+
+const iterator1 = array8.entries();
+
+console.log(iterator1.next().value);
+// expected output: Array [0, "a"]
+
+console.log(iterator1.next().value);
+// expected output: Array [1, "b"]
+
+// every() method should return
+//The every() method tests whether all elements in the array pass the test implemented by the provided function.
+// It returns a Boolean value.
+
+const isBelowThreshold = (currentValue) => currentValue < 40;
+
+const array9 = [1, 30, 39, 29, 10, 13];
+
+console.log(array9.every(isBelowThreshold));
