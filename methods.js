5327493a39ed997e2d65b7ee48b25a971a2136ed
Dulatto <dulat.omarov@gmail.com>
2022-01-04 20:39:25 -0500

    array methods

---
+++ methods.js
@@ -0,0 +1,12 @@
+//Array methods
+//Length method
+var items = ['shoes', 'shirts', 'socks', 'sweaters'];
+items.length;
+//console.log(items.length);
+
+// Concat() method
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+//console.log(array3);
