ef966dd44e94cdde4d442230af5269061dd87b41
Dulatto <dulat.omarov@gmail.com>
2022-01-05 22:03:28 -0500

    array methods

--- methods.js
+++ methods.js
@@ -70,3 +70,14 @@ console.log(array10.fill(6));
 //fill(value)
 //fill(value, start)
 //fill(value, start, end)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// filter() method
+// The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
