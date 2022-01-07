8d5a08e22ac1a4d541f992870c3b582a00eaf11c
Dulatto <dulat.omarov@gmail.com>
2022-01-06 22:17:35 -0500

    array methods

--- methods.js
+++ methods.js
@@ -164,3 +164,11 @@ console.log(beasts.indexOf('bison', 2));
 
 console.log(beasts.indexOf('giraffe'));
 // expected output: -1
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//isArray() method
+//The Array.isArray() method determines whether the passed value is an Array.
+Array.isArray([1, 2, 3]);  // true
+Array.isArray({ foo: 123 }); // false
+Array.isArray('foobar');   // false
+Array.isArray(undefined);  // false
