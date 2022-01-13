3aaa881ad819067d321525f96864c656da2b22e0
Dulatto <dulat.omarov@gmail.com>
2022-01-12 22:59:43 -0500

    array methods

--- methods.js
+++ methods.js
@@ -322,3 +322,26 @@ console.log(array20);
 
 console.log(firstElement);
 // expected output: 1
+
+//////////////////////////////////////////////////////////////////////////////////////
+//slice() method
+//The slice() method returns a shallow copy of a portion of an array into a new array 
+//object selected from start to end (end not included) where start and end represent the index of items in that array.
+// The original array will not be modified.
+
+const animalsss = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animalsss.slice(2));
+// expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animalsss.slice(2, 4));
+// expected output: Array ["camel", "duck"]
+
+console.log(animalsss.slice(1, 5));
+// expected output: Array ["bison", "camel", "duck", "elephant"]
+
+console.log(animalsss.slice(-2));
+// expected output: Array ["duck", "elephant"]
+
+console.log(animalsss.slice(2, -1));
+// expected output: Array ["camel", "duck"]
